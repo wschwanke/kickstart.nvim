3ca0753666d05dfcd0a5e916a5ce3596bb7f4cd7
Will Schwanke <wschwanke@gmail.com>
2024-03-26 10:38:52 -0500

    Fix some configs and install editconfig

---
+++ lua/plugins/editorconfig.lua
@@ -0,0 +1,6 @@
+return {
+  'editorconfig/editorconfig-vim',
+  config = function()
+    vim.g.EditorConfig_exclude_patterns = { 'fugitive://.*' }
+  end,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -43,30 +43,16 @@ return { -- Highlight, edit, and navigate code
     auto_install = true,
     highlight = {
       enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
+      use_languagetree = true,
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    indent = {
+      enable = true,
+    },
   },
   config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    ---@class ParserInfo[]
-    local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-    parser_configs.blade =
-      {
-        install_info = {
-          url = 'https://github.com/EmranMR/tree-sitter-blade',
-          files = { 'src/parser.c' },
-          branch = 'main',
-          generate_requires_npm = false,
-          requires_generate_from_grammar = true,
-        },
-        filetype = 'blade',
-      },
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup(opts)
 
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
