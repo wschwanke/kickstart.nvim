db885d40053b4e76a1d3002c26befc27c66ea759
Will Schwanke <wschwanke@gmail.com>
2024-04-15 09:25:05 -0500

    Options changes

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,7 @@
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.opt.number = true
