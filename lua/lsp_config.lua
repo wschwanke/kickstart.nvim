29b4c2ea7542b25a24a0f483e1c4b7f77f5a76f7
Will Schwanke <wschwanke@gmail.com>
2024-06-01 22:48:37 -0500

    Update stylelua and lsp config

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -47,6 +47,22 @@ return {
           }
         end,
 
+        ['yamlls'] = function()
+          lspconfig.yamlls.setup {
+            capabilities = capabilities,
+            on_attach = function(client)
+              client.server_capabilities.documentFormattingProvider = true
+            end,
+            settings = {
+              yaml = {
+                schemas = {
+                  ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible.json"] = "/**/playbooks/**/*.yml"
+                }
+              }
+            }
+          }
+        end,
+
         ['tailwindcss'] = function()
           lspconfig.tailwindcss.setup {
             capabilities = capabilities,
