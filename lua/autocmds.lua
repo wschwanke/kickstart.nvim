be00897d83dae692dbba422410edaeba9d4d5422
Will Schwanke <wschwanke@gmail.com>
2024-12-24 15:10:39 -0600

    New more streamlined config

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,52 +1,49 @@
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local ThePrimeagenGroup = augroup("ThePrimeagen", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 
 function R(name)
-  require('plenary.reload').reload_module(name)
+  require("plenary.reload").reload_module(name)
 end
 
-autocmd('LspAttach', {
+autocmd("LspAttach", {
   group = ThePrimeagenGroup,
   callback = function(e)
     local opts = { buffer = e.buf }
-    vim.keymap.set('n', 'gd', function()
+    vim.keymap.set("n", "gd", function()
       vim.lsp.buf.definition()
     end, opts)
-    vim.keymap.set('n', 'K', function()
+    vim.keymap.set("n", "K", function()
       vim.lsp.buf.hover()
     end, opts)
-    vim.keymap.set('n', '<leader>vws', function()
+    vim.keymap.set("n", "<leader>vws", function()
       vim.lsp.buf.workspace_symbol()
     end, opts)
-    vim.keymap.set('n', '<leader>vd', function()
+    vim.keymap.set("n", "<leader>vd", function()
       vim.diagnostic.open_float()
     end, opts)
-    vim.keymap.set('n', '<leader>vca', function()
+    vim.keymap.set("n", "<leader>vca", function()
       vim.lsp.buf.code_action()
     end, opts)
-    vim.keymap.set('n', '<leader>vrr', function()
+    vim.keymap.set("n", "<leader>vrr", function()
       vim.lsp.buf.references()
     end, opts)
-    vim.keymap.set('n', '<leader>vrn', function()
+    vim.keymap.set("n", "<leader>vrn", function()
       vim.lsp.buf.rename()
     end, opts)
-    vim.keymap.set('i', '<C-h>', function()
+    vim.keymap.set("i", "<C-h>", function()
       vim.lsp.buf.signature_help()
     end, opts)
   end,
--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -1,100 +1,123 @@
 return {
-  'neovim/nvim-lspconfig',
+	"neovim/nvim-lspconfig",
 
-  dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'L3MON4D3/LuaSnip',
-    'j-hui/fidget.nvim',
-    'saghen/blink.cmp',
-  },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"L3MON4D3/LuaSnip",
+		"j-hui/fidget.nvim",
+		"saghen/blink.cmp",
+		"creativenull/efmls-configs-nvim",
+	},
 
-  config = function()
-    local lspconfig = require 'lspconfig'
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-    require('fidget').setup {}
-    require('mason').setup()
-    require('mason-lspconfig').setup {
-      ensure_installed = {
-        'lua_ls',
-        'gopls',
-        'htmx',
-        'jsonls',
-        'tailwindcss',
-        'eslint',
-        'emmet_ls',
-        'rust_analyzer',
-        'ts_ls',
-        'csharp_ls',
-      },
-      handlers = {
-        function(server_name) -- default handler (optional)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-          }
-        end,
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"gopls",
+				"htmx",
+				"jsonls",
+				"tailwindcss",
+				"emmet_ls",
+				"rust_analyzer",
+				"ts_ls",
+				"csharp_ls",
+				"efm",
+			},
+			handlers = {
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
 
-        ['yamlls'] = function()
-          lspconfig.yamlls.setup {
-            capabilities = capabilities,
-            on_attach = function(client)
-              client.server_capabilities.documentFormattingProvider = true
-            end,
-            settings = {
-              yaml = {
-                schemas = {
-                  ['https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible.json'] = '/**/playbooks/**/*.yml',
-                },
-              },
-            },
-          }
-        end,
+				["efm"] = function()
+					local languages = require("efmls-configs.defaults").languages()
 
-        ['tailwindcss'] = function()
-          lspconfig.tailwindcss.setup {
-            capabilities = capabilities,
-            settings = {
-              tailwindCSS = {
-                experimental = {
-                  classRegex = {
-                    'class:\\s*?["\'`]([^"\'`]*).*?,',
-                    'clsx\\(([^)]*)\\)',
-                    'cva\\(([^)]*)\\)',
-                    'cn\\(([^)]*)\\)',
-                    '(class|className)=["\'`]([^"\'`]*).*?["\'`]',
-                  },
-                },
-              },
-            },
-          }
-        end,
+					lspconfig.efm.setup({
+						filetypes = { "lua", "typescript", "javascript", "typescriptreact", "javascriptreact" },
+						init_options = {
+							documentFormatting = true,
+							documentRangeFormatting = true,
+						},
+						settings = {
+							rootMarkers = { ".git/" },
+							languages = languages,
+						},
+						capabilities = capabilities,
+					})
+				end,
 
-        ['lua_ls'] = function()
-          lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { 'vim', 'it', 'describe', 'before_each', 'after_each' },
-                },
-              },
-            },
-          }
-        end,
-      },
-    }
+				["yamlls"] = function()
+					lspconfig.yamlls.setup({
+						capabilities = capabilities,
+						on_attach = function(client)
+							client.server_capabilities.documentFormattingProvider = true
+						end,
+						settings = {
+							yaml = {
+								schemas = {
+									["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible.json"] =
+									"/**/playbooks/**/*.yml",
+								},
+							},
+						},
+					})
+				end,
 
-    vim.diagnostic.config {
-      -- update_in_insert = true,
-      float = {
-        focusable = false,
-        style = 'minimal',
-        border = 'rounded',
-        source = 'always',
-        header = '',
-        prefix = '',
-      },
-    }
-  end,
+				["tailwindcss"] = function()
+					lspconfig.tailwindcss.setup({
+						capabilities = capabilities,
+						settings = {
+							tailwindCSS = {
+								experimental = {
+									classRegex = {
+										"class:\\s*?[\"'`]([^\"'`]*).*?,",
+										"clsx\\(([^)]*)\\)",
+										"cva\\(([^)]*)\\)",
+										"cn\\(([^)]*)\\)",
+										"(class|className)=[\"'`]([^\"'`]*).*?[\"'`]",
+									},
+								},
+							},
+						},
+					})
+				end,
+
+				["lua_ls"] = function()
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim", "it", "describe", "before_each", "after_each" },
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
+
+		vim.diagnostic.config({
+			-- update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+
+		vim.keymap.set("n", "<leader>f", function()
+			vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+		end)
+	end,
 }
