f3d630b6cba47c80aed2b1e8d47ce429b227086f
Will Schwanke <wschwanke@gmail.com>
2025-01-08 12:39:27 -0600

    Update blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,30 +1,20 @@
 return {
-	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
-	dependencies = "rafamadriz/friendly-snippets",
-
-	-- use a release tag to download pre-built binaries
-	version = "*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
+	'saghen/blink.cmp',
+	dependencies = { 'rafamadriz/friendly-snippets' },
+	-- version = '0.9.3',
+	version = '*',
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		-- 'default' for mappings similar to built-in completion
-		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-		-- See the full "keymap" documentation for information on defining your own keymap.
-		keymap = { preset = "default" },
+		keymap = { preset = 'default' },
 
 		completion = {
 			menu = {
 				draw = {
 					columns = {
-						{ "kind_icon" },
-						{ "label",    "label_description", "source_file", gap = 1 },
+						{ 'kind_icon' },
+						{ 'label',    'label_description', 'source_file', gap = 1 },
 					},
 					components = {
 						source_file = {
@@ -35,7 +25,7 @@ return {
 									return ctx.item.documentation
 								end
 							end,
-							highlight = "BlinkCmpSource", -- Define a highlight group for styling
+							highlight = 'BlinkCmpSource', -- Define a highlight group for styling
 						},
 					},
 				},
@@ -43,20 +33,13 @@ return {
 		},
 
 		appearance = {
-			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- Useful for when your theme doesn't support blink.cmp
-			-- Will be removed in a future release
 			use_nvim_cmp_as_default = true,
-			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
+			nerd_font_variant = 'mono',
 		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
 		},
 	},
-	opts_extend = { "sources.default" },
+	opts_extend = { 'sources.default' },
 }
