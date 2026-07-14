5bff3bb2cef601ee6c4fdfea2303d39af6b7f3ce
Will Schwanke <wschwanke@gmail.com>
2026-07-14 14:09:16 -0500

    Before changes

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -7,10 +7,16 @@ return {
     },
     ft = { "org" },
     config = function()
-      -- Setup orgmode
       require("orgmode").setup({
-        org_agenda_files = "~/orgfiles/**/*",
-        org_default_notes_file = "~/orgfiles/refile.org",
+        org_todo_keywords = { "TODO(t)", "NEXT(n)", "WAITING(w)", "|", "DONE(d)", "CANCELLED(c)" },
+        org_agenda_files = "~/.org/**/*",
+        org_default_notes_file = "~/.org/notes.org",
+        org_archive_location = "~/.org/archive.org",
+        org_capture_templates = {
+          t = { description = "Task", template = "* TODO %?\n  %u" },
+        },
+        org_startup_indented = true,
+        org_adapt_indentation = false,
       })
 
       vim.lsp.enable("org")
@@ -33,10 +39,16 @@ return {
       local tom = require("telescope-orgmode")
       tom.setup({ adapter = "snacks" })
 
-      vim.keymap.set("n", "<leader>osh", tom.search_headings, { desc = "[O]rg Headlines" })
-      vim.keymap.set("n", "<leader>ost", tom.search_tags, { desc = "[O]rg Tags" })
+      vim.keymap.set("n", "<leader>soh", tom.search_headings, { desc = "[O]rg Headlines" })
+      vim.keymap.set("n", "<leader>sot", tom.search_tags, { desc = "[O]rg Tags" })
       vim.keymap.set("n", "<leader>or", tom.refile_heading, { desc = "[O]rg Refile" })
-      vim.keymap.set("n", "<leader>oli", tom.insert_link, { desc = "[O]rg [L]ink [I]nsert" })
+      vim.keymap.set("n", "<leader>ol", tom.insert_link, { desc = "[O]rg [L]ink [I]nsert" })
+      vim.keymap.set("n", "<leader>of", function()
+        require("snacks").picker.files({ cwd = vim.fn.expand("~/.org") })
+      end, { desc = "[O]rg [F]iles" })
+      vim.keymap.set("n", "<leader>oe", function()
+        vim.cmd.Explore(vim.fn.expand("~/.org"))
+      end, { desc = "[O]rg [E]xplore" })
     end,
   },
 }
